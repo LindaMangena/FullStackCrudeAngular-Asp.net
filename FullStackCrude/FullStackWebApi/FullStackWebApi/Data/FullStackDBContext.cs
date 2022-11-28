de71cd8cd05d9c4ceff520ef3ff8b5ed08b1b57e
LindaMangena <lindajazz42@gmail.com>
2022-11-28 11:10:11 +0200

    Full stack crude application Angular and asp.net

---
+++ FullStackCrude/FullStackWebApi/FullStackWebApi/Data/FullStackDBContext.cs
@@ -0,0 +1,15 @@
+﻿using FullStackWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+//using System.Data.Entity;
+
+namespace FullStackWebApi.Data
+{
+    public class FullStackDBContext : DbContext
+    {
+        public FullStackDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ FullStackCrude/FullStackWebApi/FullStackWebApi/Migrations/20221031095203_Updating database values.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FullStackWebApi.Migrations
+{
+    public partial class Updatingdatabasevalues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Department",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddColumn<long>(
+                name: "Salary",
+                table: "Employees",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salary",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Department",
+                table: "Employees",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
